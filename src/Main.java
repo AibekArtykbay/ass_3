c612aaac68f20d8b226826a640cff86b0f8c4c26
AibekArtykbay <201475@astanait.edu.kz>
2021-10-05 23:06:14 +0600

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-     Doctor doctor = new NurseDecorator(new DoctorDecorator(new HeadDoctor()));
+     Doctor doctor = new HeadDoctor(new DoctorDecorator(new Paramedic()));
         System.out.println(doctor.makeJob());
     }
 }
